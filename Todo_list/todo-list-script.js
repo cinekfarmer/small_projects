ade2035596ebf24e6582baefca799761232e5106
cinekfarmer <cinekfarmer@gmail.com>
2024-11-22 23:30:02 +0100

    Now each task can be done and deleted

--- Todo_list/todo-list-script.js
+++ Todo_list/todo-list-script.js
@@ -2,6 +2,7 @@ const INPUT = document.querySelector('#text-input');
 const INPUT_BTN = document.querySelector('#input-btn');
 const OUTPUT = document.querySelector('#output');
 
+
 INPUT_BTN.addEventListener('click', () => addNewTask());
 INPUT.addEventListener('keyup', () => {
     if (event.key == "Enter") {
@@ -9,8 +10,13 @@ INPUT.addEventListener('keyup', () => {
     }
 })
 
-
 let taskList = [];
+
+run()
+
+function run() {
+    addListeners();
+}
 
 function addNewTask() {
     if (INPUT.value.trim().length == 0) {
@@ -20,14 +26,18 @@ function addNewTask() {
     addTask(INPUT.value);
     clearTextInput();
     showList(taskList);
+    addListeners();
 }
 
-function markTask() {
-    
+function markTask(task) {
+    task.firstElementChild.classList.toggle('done');
 }
 
-function deleteTask() {
-
+function deleteTask(task) {
+    let taskId = task.parentElement.id;
+    taskList.splice(taskId, 1);
+    showList(taskList);
+    addListeners();
 }
 
 function addTask(task) {    
@@ -41,11 +51,29 @@ function clearTextInput() {
 function showList(list) {
     let html = "";
     for (i=0; i < list.length; i++) {
-        html += `<li id="${i}" class="task"><span class="undone">${list[i]} </span><span class="icon-trash"></span></li>`
+        html += `
+        <li id="${i}" class="task">
+            <span class="undone">${list[i]} </span>
+            <span id="deleteBtn" class="icon-trash">
+        </span></li>`
     }
     OUTPUT.innerHTML = html;
 }
 
+function addListeners() {
+    const TASK_LIST = document.getElementsByClassName('task');
+    for (i=0; i < TASK_LIST.length; i++) {
+        let task = TASK_LIST[i];
+        task.addEventListener('click', () => markTask(task));
+    }
+    const DELETE_LIST = document.getElementsByClassName('icon-trash');
+    for (i=0; i < DELETE_LIST.length; i++) {
+        let task = DELETE_LIST[i];
+        task.addEventListener('click', () => deleteTask(task));
+    }
+}
+
+//debugging
 function call(checkpoint) {
     alert('Succes! ' + checkpoint);
 }
